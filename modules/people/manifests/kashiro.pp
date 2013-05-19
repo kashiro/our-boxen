0599a21b790475b35fb6ea87425d861324e6d1ad
kashiro <ksrtmyk@gmail.com>
2013-05-19 12:53:14 +0900

    Update kashiro.pp

--- modules/people/manifests/kashiro.pp
+++ modules/people/manifests/kashiro.pp
@@ -12,6 +12,7 @@ class people::kashiro {
   include mou
   include zsh
 
+  # homebrewでインストール
   package {
     'Kobito':
       source   => "http://kobito.qiita.com/download/Kobito_v1.5.0.zip",
@@ -19,7 +20,13 @@ class people::kashiro {
     'XtraFinder':
       source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
       provider => pkgdmg;
+    'spectacle':
+      source   => "https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.2.zip",
+      provider => compressed_app;
+    [
+      'tig',
+      'svn'
+    ]
   }
-
 }
 
