36f7b80e7f8e537a4c3eb333c7f581b65389f1e5
kashiro <ksrtmyk@gmail.com>
2013-05-19 13:07:01 +0900

    Update kashiro.pp

--- modules/people/manifests/kashiro.pp
+++ modules/people/manifests/kashiro.pp
@@ -23,6 +23,12 @@ class people::kashiro {
     'spectacle':
       source   => "https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.2.zip",
       provider => compressed_app;
+    'LimeChat':
+      source   => "https://downloads.sourceforge.net/project/limechat/limechat/LimeChat_2.34.tbz?use_mirror=master",
+      provider => compressed_app;
+    'GoogleJapaneseInput':
+      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      provider => pkgdmg;
     [
       'tig',
       'svn'
